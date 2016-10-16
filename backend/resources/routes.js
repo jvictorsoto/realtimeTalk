19cf73c76c991f7bea46bf0632982fdbbc894087
Victor Soto <vktrsm@gmail.com>
2016-10-16 21:06:55 +0200

    refactor(routes): Backend: Changed API routes to support chats

--- backend/resources/routes.js
+++ backend/resources/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import chatsRoutes from './chats/chats.routes';
 import messagesRoutes from './messages/messages.routes';
 import usersRoutes from './users/users.routes';
 import authenticator from '../utils/authenticator';
 
-const router = express.Router()
+const router = express.Router(); // eslint-disable-line new-cap
 
-router.use('/messages', authenticator.ensureAuthenticated, messagesRoutes);
+router.use('/chats', authenticator.ensureAuthenticated, chatsRoutes);
+router.use('/chats', authenticator.ensureAuthenticated, messagesRoutes);
 router.use('/users', usersRoutes);
 
 export default router;
